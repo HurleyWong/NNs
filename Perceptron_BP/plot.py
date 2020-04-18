3fe724055fd76c0d4111ce0fb6b40eedb61563a3
Hurley <1401682479@qq.com>
2020-04-18 18:39:39 +0100

    add perceptron part of three classes

--- Perceptron_BP/plot.py
+++ Perceptron_BP/plot.py
@@ -25,19 +25,6 @@ petal_length_ticks = np.arange(1, 7, step=1)
 ticks = [sepal_length_ticks, sepal_width_ticks, petal_length_ticks, petal_width_ticks]
 label_text = ['Sepal Length', 'Sepal Width', 'Petal Length', 'Petal Width']
 
-# iris_setosa = np.hsplit(iris_setosa, 4)
-# iris_versicolor = np.hsplit(iris_versicolor, 4)
-# iris_virginica = np.hsplit(iris_virginica, 4)
-
-# setosa = {'sepal_length': iris_setosa[0], 'sepal_width': iris_setosa[1],
-#           'petal_length': iris_setosa[2], 'petal_width': iris_setosa[3]}
-#
-# versicolor = {'sepal_length': iris_versicolor[0], 'sepal_width': iris_versicolor[1],
-#               'petal_length': iris_versicolor[2], 'petal_width': iris_versicolor[3]}
-#
-# virginica = {'sepal_length': iris_virginica[0], 'sepal_width': iris_virginica[1],
-#              'petal_length': iris_virginica[2], 'petal_width': iris_virginica[3]}
-
 plt.figure(figsize=(12, 12))
 
 # plt.scatter(x=iris_setosa.iloc[:, 3], y=iris_setosa.iloc[:, 2], color=setosa_color, s=size)
@@ -60,13 +47,13 @@ for i in range(0, 4):
             plt.yticks([])
             plt.text(0.1, 0.4, label_text[i], size=18)
         else:
-            plt.scatter(x=iris_setosa.iloc[:, j], y=iris_setosa.iloc[:, i], color=setosa_color, s=size)
-            plt.scatter(x=iris_versicolor.iloc[:, j], y=iris_versicolor.iloc[:, i], color=versicolor_color, s=size)
-            plt.scatter(x=iris_virginica.iloc[:, j], y=iris_virginica.iloc[:, i], color=virginica_color, s=size)
+            plt.scatter(x=iris_setosa.iloc[:, j], y=iris_setosa.iloc[:, i], color=setosa_color, s=size, label='setosa')
+            plt.scatter(x=iris_versicolor.iloc[:, j], y=iris_versicolor.iloc[:, i], color=versicolor_color, s=size, label='versicolor')
+            plt.scatter(x=iris_virginica.iloc[:, j], y=iris_virginica.iloc[:, i], color=virginica_color, s=size, label='virginica')
             plt.title('{} vs {}'.format(label_text[i], label_text[j]))
             plt.xlabel(label_text[j])
             plt.ylabel(label_text[i])
             plt.xticks(ticks[j])
             plt.yticks(ticks[i])
 
-plt.savefig('first.png', format='png')
+plt.savefig('iris.png', format='png')
